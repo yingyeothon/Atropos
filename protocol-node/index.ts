1fadc26fe37ef770d938efeb04c0dccd5ccb6a2a
Dongwoon Son <dplusic@gmail.com>
2020-07-28 12:14:35 +0900

    Create packages structure

---
+++ protocol-node/index.ts
@@ -0,0 +1,3 @@
+export type Test = {}
+
+export const VERSION = '1.0.0'
---
+++ server-node/index.ts
@@ -0,0 +1,5 @@
+import { Test, VERSION } from '../protocol-node'
+
+const test: Test = {}
+console.log(test)
+console.log(VERSION)
