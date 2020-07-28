51bf1bd41869736e2838909fe1a0987a22c4a011
jaeyoung.choi <lactrious@gmail.com>
2020-07-28 12:42:43 +0900

    Add parseIdMoveMessages

--- protocol-node/index.ts
+++ protocol-node/index.ts
@@ -2,11 +2,14 @@ export type Test = {};
 
 export const VERSION = "1.0.0";
 
+export type Pos = [number, number];
+export type IdPos = [string, number, number];
+
 export function serializeMoveMessage(x: number, y: number): string {
   return `${x} ${y}`;
 }
 
-export function parseMoveMessage(data: string): [number, number] | null {
+export function parseMoveMessage(data: string): Pos | null {
   const index = data.indexOf(" ");
   if (index < 0) {
     return null;
@@ -24,9 +27,7 @@ export function serializeIdMoveMessage(
   return `${id} ${x} ${y}`;
 }
 
-export function parseIdMoveMessage(
-  data: string
-): [string, number, number] | null {
+export function parseIdMoveMessage(data: string): IdPos | null {
   const first = data.indexOf(" ");
   if (first < 0) {
     return null;
@@ -39,4 +40,18 @@ export function parseIdMoveMessage(
   const x = +data.substring(first + 1, second);
   const y = +data.substring(second + 1);
   return isNaN(x) || isNaN(y) || x === null || y === null ? null : [id, x, y];
+}
+
+export function parseIdMoveMessages(data: string): IdPos[] {
+  let lastIndex = 0;
+  const result: IdPos[] = [];
+  while (true) {
+    const nextIndex = data.indexOf("\n", lastIndex + 1);
+    const endIndex = nextIndex < 0 ? data.length : nextIndex;
+    result.push(parseIdMoveMessage(data.substring(lastIndex, endIndex)));
+    if (nextIndex < 0) {
+      return result;
+    }
+    lastIndex = nextIndex + 1;
+  }
 }
